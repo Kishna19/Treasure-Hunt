c52ea578cc294ef28b9945d0d4cfa8b803f40da0
Kishna19 <74139045+Kishna19@users.noreply.github.com>
2021-05-23 21:24:30 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() 
+{
+    
+    fill("red")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("yellow")
+    text("Hint: Always changing, not constant !!", 100,70)
+  // add code for displaying rest of the names and the hints.
+    
+    fill("red")
+    textSize(15)
+    text("N U I T F C N O", 700,150)
+    fill("yellow")
+    text("Hint: a set of statements that performs a task or calculates a value", 550,170)
+
+    fill("red")
+    textSize(15)
+    text("E A A D S T A B", 100,200)
+    fill("yellow")
+    text("Hint: an organized collection of structured information, or data", 100,220)
+
+}
